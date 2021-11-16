e5671dd4b9a13254ca83ed5466fe34bbadabe884
Leweyse <darylcastro67@gmail.com>
2021-11-16 12:20:01 +0100

    defined variables and copied elements in array

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ import mainContent from './pages/main.js'
 
 mainContent();
 
+let main = document.getElementById('main');
+let box = document.querySelector('.box');
+let result = document.getElementById('result');
+let runBtn = document.getElementById('run');
+
+let copyResult = null;
+
+let interval = null;
+
+let arr = [...box.childNodes];
+
