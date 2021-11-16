67d7dd50a1520d2d3bcb14a5178c11e1e9ea83b7
Leweyse <darylcastro67@gmail.com>
2021-11-16 12:20:25 +0100

    implemented logic for Run button

--- js/index.js
+++ js/index.js
@@ -14,3 +14,36 @@ let interval = null;
 
 let arr = [...box.childNodes];
 
+runBtn.addEventListener('click', function() {
+    if (result.childNodes.length < 6) {
+        runBtn.style.pointerEvents = 'all';
+
+        let arr = [...box.childNodes];
+        let random = getRandom(arr, 1);
+
+        clearInterval(interval);
+
+        if (result.childNodes.length < 5) {
+            runBtn.style.pointerEvents = 'none';
+            interval = setInterval(() => {
+                runBtn.click();
+            }, 500);
+        }
+
+        result.appendChild(document.getElementById(`${random[0].id}`));
+    } else {
+        box.innerHTML = "";
+
+        copyResult = result.cloneNode(true);
+        
+        main.appendChild(copyResult);
+
+        arr.map(elem => {
+            box.appendChild(elem);
+        })
+        
+        runBtn.click();
+
+        result.innerHTML = "";
+    }
+});
