2674a0f35ba28ec2e287ad6236c563fe3b579911
Leweyse <darylcastro67@gmail.com>
2021-11-17 09:03:53 +0100

    implemented input logic

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 import getRandom from './helpers/getRandom.js';
 import mainContent from './pages/main.js'
-import table from './components/sections/table.section.js';
 
 mainContent();
 
-let main = document.getElementById('main');
-let box = document.querySelector('.box');
 let result = document.getElementById('result');
+let box = document.querySelector('.box');
+let table = document.querySelector('.table');
 
 let input = document.getElementById('number');
 let runBtn = document.getElementById('run');
@@ -19,29 +18,30 @@ let interval = null;
 let originalNode = [...box.childNodes];
 
 runBtn.addEventListener('click', function() {
-    if (result.childNodes.length < 6) {
-        runBtn.style.pointerEvents = 'all';
+    runBtn.style.pointerEvents = 'all';
 
-        let arr = [...box.childNodes];
-        let random = getRandom(arr, 1);
+    let arr = [...box.childNodes];
+    let random = getRandom(arr, 1);
 
-        clearInterval(interval);
+    clearInterval(interval);
 
-        if (result.childNodes.length < 5) {
-            runBtn.style.pointerEvents = 'none';
-            interval = setInterval(() => {
-                runBtn.click();
-            }, 500);
-        }
+    if (result.childNodes.length < 5) {
+        runBtn.style.pointerEvents = 'none';
+        interval = setInterval(() => {
+            runBtn.click();
+        }, 500);
+    }
 
-        result.appendChild(document.getElementById(`${random[0].id}`));
-    } else {
+    result.appendChild(document.getElementById(`${random[0].id}`));
+
+    if (result.childNodes.length >= 6) {
+
         box.innerHTML = "";
 
         copyResult = result.cloneNode(true);
         copyResult.removeAttribute('id');
 
-        document.querySelector('.table').appendChild(copyResult);
+        table.appendChild(copyResult);
 
         originalNode.map(elem => {
             box.appendChild(elem);
@@ -58,5 +58,10 @@ input.addEventListener('keydown', function (event) {
 })
 
 hiddenBtn.addEventListener('click', function() {
-    
+    let value = input.value;
+    hiddenBtn.style.pointerEvents = 'none';
+
+    for (let i = 0; i < (value * 6); i++) {
+        runBtn.click();
+    }
 })
